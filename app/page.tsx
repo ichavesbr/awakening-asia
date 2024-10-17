3f965683bea2201c06302797eecf0d110ce7f87a
Igor Chaves Donega <ichaves@live.com>
2024-10-18 01:56:58 +0900

    added flex:1 in globals.css; imported Image from next/image on main page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { SchoolAASM } from "@/components/school-aasm"
 import { DonationCards } from "@/components/donation-cards"
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -25,31 +26,31 @@ export default function Home() {
       </section> */}
 
       {/* <section className="home-section a2">
-          <div className="home-section2-outer-wrapper">
-            <div className="home-section2-inner-wrapper-title">
-              <h3>God is Awakening His People in Asia</h3>
+        <div className="home-section2-outer-wrapper">
+          <div className="home-section2-inner-wrapper-title">
+            <h3>God is Awakening His People in Asia</h3>
+          </div>
+          <div className="home-section2-inner-wrapper">
+            <div className="home-section2-img-wrapper">
+              <Image src="/home1.png" alt="home image" height={300} width={400} />
             </div>
-            <div className="home-section2-inner-wrapper">
-              <div className="home-section2-img-wrapper">
-                <Image src="/home1.png" alt="home image" height={300} width={400} />
-              </div>
-              <div className="home-section2-txt-wrapper">
-                <p>
-                  We&apos;re an organization that&apos;s raising up the generations of life surrendered revivalists,
-                  who&apos;ll radically bring the gospel to the nations and lead Jesus movements. We believe harvest is
-                  ripe now in Asia and he is raising up heathy pioneering leaders, churches and ministries that&apos;ll
-                  transform and impact the societies with the power of the gospel.
-                </p>
-                <div className="home-section2-btns-wrapper">
-                  <a href="">link1</a>
-                  <a href="">link2</a>
-                </div>
+            <div className="home-section2-txt-wrapper">
+              <p>
+                We&apos;re an organization that&apos;s raising up the generations of life surrendered revivalists,
+                who&apos;ll radically bring the gospel to the nations and lead Jesus movements. We believe harvest is
+                ripe now in Asia and he is raising up heathy pioneering leaders, churches and ministries that&apos;ll
+                transform and impact the societies with the power of the gospel.
+              </p>
+              <div className="home-section2-btns-wrapper">
+                <a href="">link1</a>
+                <a href="">link2</a>
               </div>
             </div>
           </div>
-        </section>
-        <section className="home-section a3">ONLINE SERVICE</section>
-        <section className="home-section a4">SCHOOL</section> */}
+        </div>
+      </section>
+      <section className="home-section a3">ONLINE SERVICE</section>
+      <section className="home-section a4">SCHOOL</section> */}
     </>
   )
 }
