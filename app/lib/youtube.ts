bf47d694e974b4372d5419e43a9fb228090cd901
Igor Chaves Donega <236210133+ichavesbr@users.noreply.github.com>
2026-05-09 17:21:25 +0900

    resctructure youtube.ts

--- app/lib/youtube.ts
+++ app/lib/youtube.ts
@@ -1,10 +1,14 @@
-const getLastestVideos = async () => {
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+const CHANNEL_ID = process.env.NEXT_PUBLIC_CHANNEL_ID
+const urlBase = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}`
+const videosUrlQueries = "&part=snippet&order=date&maxResults=8&type=video"
+const livesUrlQueries = "&part=snippet&order=date&maxResults=4&type=video&eventType=completed"
+
+const fetchData = async (urlQueries: string) => {
   try {
-    const urlBase = "https://www.googleapis.com/youtube/v3/search?"
-    const urlQueries = `key=${process.env.NEXT_PUBLIC_API_KEY}&channelId=${process.env.NEXT_PUBLIC_CHANNEL_ID}&part=snippet&order=date&maxResults=8&type=video`
     const res = await fetch(urlBase + urlQueries)
 
-    if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`)
+    if (!res.ok) throw new Error("Failed to fetch YouTube data")
 
     const data = await res.json()
 
@@ -15,21 +19,7 @@ const getLastestVideos = async () => {
   }
 }
 
-const getLastestLives = async () => {
-  try {
-    const urlBase = "https://www.googleapis.com/youtube/v3/search?"
-    const urlQueries = `key=${process.env.NEXT_PUBLIC_API_KEY}&channelId=${process.env.NEXT_PUBLIC_CHANNEL_ID}&part=snippet&order=date&maxResults=4&type=video&eventType=completed`
-    const res = await fetch(urlBase + urlQueries)
-
-    if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`)
-
-    const data = await res.json()
-
-    return data.items
-  } catch (error) {
-    console.log(error)
-    return []
-  }
-}
+const getLastestVideos = async () => fetchData(videosUrlQueries)
+const getLastestLives = async () => fetchData(livesUrlQueries)
 
 export { getLastestVideos, getLastestLives }
